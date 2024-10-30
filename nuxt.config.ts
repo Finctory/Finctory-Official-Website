6d3efa8734b409edbf09dcb02613585dfa6b2cb4
黃鉅燊 <matchaskill@gmail.com>
2024-10-31 04:48:34 +0800

    chore: deploy setting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,8 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   modules: ['@nuxtjs/tailwindcss'],
-  ssr: true,
+  ssr: false,
   devtools: { enabled: false },
   css: ['./assets/main.css'],
+  buildAssetsDir: '/static/',
 })
